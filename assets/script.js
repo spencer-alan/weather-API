21e9d2817af16002556d924df975507be94b4fc3
b1acksmyth <smith.spence13@gmail.com>
2020-07-20 23:01:00 -0700

    found UNIX UTC converter formula, deleted moment.js, finfished 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -31,7 +31,15 @@ $(document).ready(function() {
       }).then(function(response) {
         console.log(response);
 
-        let m = moment();
+        let date = response.current.dt;
+        let milliseconds = date * 1000;
+        let dateObject = new Date(milliseconds);
+        let options = {
+          month: "numeric",
+          day: "numeric", 
+          year: "numeric"
+        }
+        let humanDate = dateObject.toLocaleString("en-US", options);
         let icon = response.current.weather[0].icon;
         let iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
         let iconAlt = response.current.weather[0].description;
@@ -40,29 +48,54 @@ $(document).ready(function() {
         let wind = "Wind Speed: " + response.current.wind_speed + " MPH";
         let uvIndex = "UV Index: " + response.current.uvi;
         
-        $("#date").html(m.format("l"));
+        $("#date").html(humanDate);
         $("#temp").html(temp);
         $("#humidity").html(humid);
         $("#wind").html(wind);
         $("#uv-index").html(uvIndex);
         $("#icon-image").attr("src", iconUrl);
         $("#icon-image").attr("alt", iconAlt);
-
+     
+        $(".card-body").html("");
         for (let i = 0; i < 5; i++) {
-          let j = 1;
-          let newDate = m.add(j, "days");
-          j++;
           
-          let cardArray = $(".card-body").toArray();
+          let card1 = $("#card1");
+          let card2 = $("#card2");
+          let card3 = $("#card3");
+          let card4 = $("#card4");
+          let card5 = $("#card5");
+          let cardArray = [card1, card2, card3, card4, card5];
 
-          let newH1 = cardArray[i].append("<h1></h1>");
-          newH1.html(newDate);
-          // let forecast = respone.daily[i];
-          // let forecastIconUrl = forecast.weather[0].icon;
-          // let forecastIconAlt = forecast.weather[0].description;
-          // let forecastTemp = forecast.temp.max;
-          // let forecastHumid = forecast.humidity;
-          
+          let forecast = response.daily[i];
+          let forecastDate = forecast.dt;
+          let forecastMilliseconds = forecastDate * 1000;
+          let forecastDateObject = new Date(forecastMilliseconds);
+          let forecastOptions = {
+            month: "numeric",
+            day: "numeric", 
+            year: "numeric"
+          }
+          let forecastHumanDate = forecastDateObject.toLocaleString("en-US", forecastOptions);
+          let forecastIcon = forecast.weather[0].icon;
+          let forecastIconUrl = "http://openweathermap.org/img/wn/" + forecastIcon + ".png";
+          let forecastIconAlt = forecast.weather[0].description;
+          let forecastTemp = forecast.temp.max;
+          let forecastHumid = forecast.humidity;
+          let newP = $("<p>");
+          let newImg = $("<img>");
+          let newDivTemp = $("<div>");
+          let newDivHumid = $("<div>");
+
+          newP.html(forecastHumanDate);
+          cardArray[i].append(newP);
+          newImg.attr("src", forecastIconUrl);
+          newImg.attr("alt", forecastIconAlt);
+          cardArray[i].append(newImg);
+          newDivTemp.html("Tempurature: " + forecastTemp + "F");
+          cardArray[i].append(newDivTemp);
+          newDivHumid.html("Humidity: " + forecastHumid);
+          cardArray[i].append(newDivHumid);
+
         }
       })
     })  
